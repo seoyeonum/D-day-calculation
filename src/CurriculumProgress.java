94c5ad74ccec7c1a8567f8dc3b0d88e3d3026503
Seoyeon Um <syum97@gmail.com>
2025-03-16 16:39:50 +0900

    add date difference calculation

--- src/CurriculumProgress.java
+++ src/CurriculumProgress.java
@@ -1,41 +1,64 @@
+
+// ■ BufferedReader 사용하여 입력값에 따른 수업 진행률 구하기
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Calendar; // Scanner도 util 상자!
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CurriculumProgress
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args) throws IOException, ParseException
 	{
-		
-		// BuffedReader 클래스 기반의 인스턴스 생성
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 변수 선언 및 초기화
-		double progress;
-		int day;
-		
-		// 연산 및 처리
-		System.out.print("누적 수업일수 N를 입력하세요(N일차): ");
-		day = Integer.parseInt(br.readLine());
-		
-		// 실 수업일수 140일
-		progress = day/140.0*100;		//-- 백분율을 『*100』 해줘야
 	
-		//결과 출력
-		//System.out.println("F 강의실 과정 진행률: " + progress);
-		System.out.printf("F 강의실 과정 진행률: %.2f%%", progress);
-		//-- 문자열 %를 문자열 내에 작성 시 앞에 % 붙이기
-		//-- 따옴표의 경우 \(back slash) 붙여주기
-		System.out.println();
-		
+	// BuffedReader 클래스 기반의 인스턴스 생성
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	// 변수 선언 및 초기화
+	String dateFormatType = "yyyyMMdd";
+	String strDate = "20240923";
+	String todayDate= "20241010";
+	String endDate = "20250422";
+	
+	// 연산 및 처리
+	System.out.print("오늘 날짜를 입력하세요(yyyyMMdd): ");
+	todayDate = br.readLine();				//-- 아직 문자열
+
+	// SimpleDateFormat 객체를 생성하고 포맷 형식을 설정합니다.
+	SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormatType);
+
+	// 시작일과 종료일 문자열을 Date 객체로 변환합니다.
+	Date from = simpleDateFormat.parse(strDate);
+	Date mid = simpleDateFormat.parse(todayDate);
+	Date to = simpleDateFormat.parse(endDate);
+	
+	//테스트(확인)
+	//System.out.println("strDate: " + strDate);
+	//System.out.println("from: " + from);
+	
+	
+	// 두 날짜의 차이를 밀리초 단위로 계산합니다.
+	long diffDate = to.getTime() - from.getTime();		//-- 날짜 간 차이(하루, 이틀...) = 마지막날 - 시작날
+
+	// 밀리초 단위의 차이를 일수로 변환합니다.
+	long period = diffDate / 86400000L;	
+	// 결과를 출력합니다.
+	System.out.println(period);							//-- 전체 기간에서 주말이랑 쉬는날이랑 공휴일 빼줘야 함@!@@@@@@@@@@
+
 	}
 }
 
 // 실행 결과
 /*
 
-누적 수업일수 N를 입력하세요(N일차): 5
-F 강의실 과정 진행률: 3.57%
+오늘 날짜를 입력하세요(yyyyMMdd): 20241015
+211
+
 
 */
